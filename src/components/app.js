9aca715f5c94aaaf1b55c9857ff76df6c604f33a
Hanran <hanrankim@gmail.com>
2018-03-21 18:28:24 -0700

    finished feature3

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
 
+import React from 'react'
+import Table from './table';
 
+export default () => {
+    return (
+    <div className="container">
+        <h1>Student Grade Table</h1>
+        <Table data = {students}/>
+    </div>
+    )
+};
+
+const students = [
+    {
+        name: 'Henry',
+        course: 'Math',
+        grade: 30
+    },
+    {
+        name: 'Alice',
+        course: 'History',
+        grade: 100
+    },
+    {
+        name: 'Steven',
+        course: 'Art',
+        grade: 78
+    }
+];
